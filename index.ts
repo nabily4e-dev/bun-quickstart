b1932a56ff138baa4b8f19e3691a985ed7e4aa2f
Nabil Al-Ashbat <nbl.dev21@gmail.com>
2023-11-05 12:39:54 +0300

    implement: finish bun docs quickstart

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import figlet from "figlet";
+
+const server = Bun.serve({
+  fetch() {
+    const body = figlet.textSync("Bun!");
+    return new Response(body);
+    return new Response("Bun!");
+  },
+  port: 3000,
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);
